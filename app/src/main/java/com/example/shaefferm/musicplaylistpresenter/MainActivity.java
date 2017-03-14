2996eccde9c859f4461b7621c388dc3167d85228
Shaeffer <shaefferm@dot.state.al.us>
2017-03-14 14:38:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/shaefferm/musicplaylistpresenter/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.shaefferm.musicplaylistpresenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//TODO Access the storage
+//TODO Scan the contents of the android music directory
+//TODO ListView to display the Artists
+//TODO 
