857ccd14dc8d8a69c29f2f8d16eb833013c9db99
Matt Shaeffer <matt@theshaeffers.com>
2017-03-14 16:13:49 -0500

    update

--- app/src/main/java/com/example/shaefferm/musicplaylistpresenter/MainActivity.java
+++ app/src/main/java/com/example/shaefferm/musicplaylistpresenter/MainActivity.java
@@ -10,9 +10,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    
 }
 
 //TODO Access the storage
-//TODO Scan the contents of the android music directory
-//TODO ListView to display the Artists
-//TODO 
+//TODO Simple UI to find, and play music
+//TODO Scan the contents of the android music directory, AudioManager
+//https://developer.android.com/guide/topics/media-apps/audio-app/building-an-audio-app.html
+//TODO ListView to display the music content (artist, album, songs)
+//TODO Options per object to add to playlist, play now, shuffle now?
+//TODO Content Resolver to share playlists to other apps?
